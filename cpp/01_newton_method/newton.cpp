9013d11d9532d1e37e01fa812b40a1d630ae5a7e
Yoonku Hwang <yz0624@gmail.com>
2017-01-23 13:10:46 +0900

    minor change

--- cpp/01_newton_method/newton.cpp
+++ cpp/01_newton_method/newton.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-	double x0 = 0.0;
+	double x0 = 1.5;
 	for (int i = 0; i < 100; ++i)
 	{
 		double fval = (x0 - 1.0) * (x0 - 2.0) * (x0 - 3.0);
 		double dfval = 3.0 * x0 * x0 - 12.0 * x0 + 11.0;
 		x0 = x0 - fval / dfval;
-		printf("%d : %4.4f", i, x0);
+		printf("%d : %4.4f\n", i, x0);
 	}
 	return 0;
 }
