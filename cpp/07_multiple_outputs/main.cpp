216cb55ea2f860c1b6f8ab16138274d568951734
Yoonku Hwang <yz0624@gmail.com>
2017-01-25 14:26:40 +0900

    question

--- cpp/07_multiple_outputs/main.cpp
+++ cpp/07_multiple_outputs/main.cpp
@@ -8,12 +8,20 @@ void f(double x, double y, double* out1, double* out2)
 	*out2 = 2 * (y - 1.0) * (y - 2.0) * (y - 3.0);
 }
 
+void f(double x, double y, double out1, double out2)
+{
+	out1 = (x - 1.0) * (x - 2.0) * (x - 3.0);
+	out2 = 2 * (y - 1.0) * (y - 2.0) * (y - 3.0);
+}
+
 int main()
 {
 	double x0 = 1.5;
 	double y0 = 1.5;
 
 	double val1 = 0.0, val2 = 0.0;
+	printf("output = (%6.6f, %6.6f)\n", val1, val2);
+	f(x0, y0, val1, val2);
 	printf("output = (%6.6f, %6.6f)\n", val1, val2);
 	f(x0, y0, &val1, &val2);
 	printf("output = (%6.6f, %6.6f)\n", val1, val2);
