657f526220fc82a9347e95eddba76ed5e2d2542a
Yoonku Hwang <yz0624@gmail.com>
2017-01-25 14:37:26 +0900

    trial 1

--- cpp/08_array_cstyle/main.cpp
+++ cpp/08_array_cstyle/main.cpp
@@ -15,6 +15,12 @@ int main()
 		a[i] = i;
 		b[i] = 2 * i;
 	}
+	
+	for (int i = 0; i < N; ++i)
+		printf("c[%d] = %4.4f\n", i, b[i]);
+	b = a;
+	for (int i = 0; i < N; ++i)
+		printf("c[%d] = %4.4f\n", i, b[i]);
 
 	// Initialize output array
 	double* c = (double*) malloc(N * sizeof(double));
