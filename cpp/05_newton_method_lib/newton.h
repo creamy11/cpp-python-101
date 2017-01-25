c1568f8fe23a1fe53406efef53a988ae26e48bc7
Yoonku Hwang <yz0624@gmail.com>
2017-01-25 16:34:38 +0900

    Revert "extern "C""
    This reverts commit eff7b5a4cdc55aee5313209ab80c022d0a2b59bf.

--- cpp/05_newton_method_lib/newton.h
+++ cpp/05_newton_method_lib/newton.h
@@ -1,8 +1,5 @@
 #include <stdio.h>
 
-extern "C"
-{
-	double df(double (*func)(double), double x, double dx);
+double df(double (*func)(double), double x, double dx);
 
-	double newton(double (*func)(double), double x0, int maxIter);
-}
+double newton(double (*func)(double), double x0, int maxIter);
