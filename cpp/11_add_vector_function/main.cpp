adefc753c0692cee89e051e28310e96d94c84db8
Yoonku Hwang <yz0624@gmail.com>
2017-01-25 13:06:10 +0900

    add argument

---
+++ cpp/11_add_vector_function/main.cpp
@@ -0,0 +1,48 @@
+// g++ main.cpp -o main.x
+
+#include <stdio.h>
+
+void f(double* in1, double* in2, double* out, int n)
+{
+	for (int i = 0; i < n; ++i)
+		out[i] = in1[i] + in2[i];
+}
+
+int main(int argc, char const *argv[])
+{
+	int N = 5;
+	if(argc > 2)
+		N = atoi(argv[1]);
+
+	double* a = new double[N];
+	double* b = new double[N];
+
+	// Initialize arrays
+	for (int i = 0; i < N; ++i)
+	{
+		a[i] = i;
+		b[i] = 2 * i;
+	}
+
+	// Initialize output array
+	double* c = new double[N];
+	for (int i = 0; i < N; ++i)
+		c[i] = 0.0;
+
+	for (int i = 0; i < N; ++i)
+		printf("c[%d] = %4.4f\n", i, c[i]);
+
+	printf("\n\n");
+	
+	// add
+	f(a, b, c, N);
+
+	for (int i = 0; i < N; ++i)
+		printf("c[%d] = %4.4f\n", i, c[i]);
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+
+	return 0;
+}
