aebb6c858f65a235ce5987da62b3825bc0dcb767
Yoonku Hwang <yz0624@gmail.com>
2017-01-25 13:12:36 +0900

    indentation

--- cpp/12_input_arguments/main.cpp
+++ cpp/12_input_arguments/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int main( int argc, char* argv[] )
-  {
-  cout << "The name used to start the program: " << argv[ 0 ]
-       << "\nArguments are:\n";
-  for (int n = 1; n < argc; n++)
-    cout << setw( 2 ) << n << ": " << argv[ n ] << '\n';
-  return 0;
-  }
+{
+	cout << "The name used to start the program: " << argv[ 0 ]
+	   << "\nArguments are:\n";
+	for (int n = 1; n < argc; n++)
+		cout << setw( 2 ) << n << ": " << argv[ n ] << '\n';
+	return 0;
+}
