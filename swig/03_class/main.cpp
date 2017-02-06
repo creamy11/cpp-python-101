30041ad498a81c2b353b0ded570a96b4d2553cd2
Yoonku Hwang <yz0624@gmail.com>
2017-02-06 12:58:43 +0900

    typo

--- swig/03_class/main.cpp
+++ swig/03_class/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[])
 {
 	printf("%s\n", "Hello world!");
-	Shape circle = new Circle(2.0);
+	Circle circle = new Circle(2.0);
 	printf("%f\n", circle.area());
 	delete circle;
 	return 0;
